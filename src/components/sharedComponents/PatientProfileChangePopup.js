cf0c612ac8dcef805fba73a352b010af6ba6cf68
sandeep <sandeep.kumar@paxcel.net>
2020-05-29 18:34:51 +0530

    popup and ticket update

--- src/components/sharedComponents/PatientProfileChangePopup.js
+++ src/components/sharedComponents/PatientProfileChangePopup.js
@@ -2,9 +2,34 @@ import React from "react";
 
 const PatientProfileChangePopup = (props) => {
   return (
-    <div >
-      <div>Changes were made to patient profile for ticket #{props.ticketNumber}. Please review the changes for this patient.</div>
-    </div>
+    <div className="content" >
+      <p> Changes were made to patient profile for ticket #{props.ticketNumber}. Please review the changes for this patient.</p>
+      
+      <div  className="patient-details-50">
+      <h2> Previous Patient Details</h2>
+        <div className="border pull-left">
+     <h2 style={{'padding-left':'10px'}}> Diet:</h2>  
+          <ul>
+            <li>Allergies:</li>
+            <li>Supplements:</li>
+            <li>Tube Feeding:</li>
+            <li>Patient Notes:</li>
+            </ul>
+          </div> </div>
+      <div  className="patient-details-50">
+      <h2>Current Patient Details</h2>
+      <div className="border pull-right">
+      <h2 style={{'padding-left':'10px'}}>  Diet: </h2>  
+      <ul>
+            <li>Allergies:</li>
+            <li>Supplements:</li>
+            <li>Tube Feeding:</li>
+            <li>Patient Notes:</li>
+            </ul>
+       </div> 
+       </div>
+      </div>
+ 
   );
 }
 PatientProfileChangePopup.defaultProps = {
