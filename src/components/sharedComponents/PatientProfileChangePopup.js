50f81df4bccb6c08cd55950ce78b81b463e0dc52
sandeep <sandeep.kumar@paxcel.net>
2020-06-16 20:27:30 +0530

    popup update

--- src/components/sharedComponents/PatientProfileChangePopup.js
+++ src/components/sharedComponents/PatientProfileChangePopup.js
@@ -6,10 +6,13 @@ const PatientProfileChangePopup = (props) => {
 
       <p> Changes were made to patient profile for ticket #{props.ticketNumber}. Please review the changes for this patient.</p>
           <h2 className="heading-text"> Previous Patient Details</h2>
-          <h2 className="heading-text">Current Patient Details</h2>
+          <h2 className="heading-text heading-2" >Current Patient Details</h2>
 
 <div  className="table-outer">
       <table className="table-alert-box border"  >
+
+   
+            
 
       <tr>
               <td>Diet:  <span> {props.oldDetails.diet}</span> </td>
@@ -18,8 +21,8 @@ const PatientProfileChangePopup = (props) => {
 
 
       <tr>
-              <td>Allergies <span> {props.oldDetails.allergies}</span> </td>
-              <td>Allergies <span> {props.newDetails.allergies}</span></td>
+              <td>Allergies: <span> {props.oldDetails.allergies}</span> </td>
+              <td>Allergies: <span> {props.newDetails.allergies}</span></td>
             </tr>
 
             <tr>
